d023928b54528bb8ed60260f81203fab8e35bb02
王文杰 <wenjie.wang@chiefclouds.com>
2018-06-14 17:48:03 +0800

    mock

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
 import { CecWebappPage } from './app.po';
 
-describe('cec-webapp App', () => {
-  let page: CecWebappPage;
+describe('app', () => {
+	let page: CecWebappPage;
 
-  beforeEach(() => {
-    page = new CecWebappPage();
-  });
+	beforeEach(() => {
+		page = new CecWebappPage();
+	});
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
-  });
+	it('should display welcome message', () => {
+		page.navigateTo();
+		expect(page.getParagraphText()).toEqual('Welcome to app!');
+	});
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,24 +6,33 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatDialogModule, MatIconModule } from '@angular/material';
 
+//todo
+import { ngMock } from 'angular-mocks/ngMock';
+
+
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
+
+
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    NotFoundComponent
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    FlexLayoutModule,
-    MatDialogModule, MatIconModule,
-    CoreModule,
-    AppRoutingModule
-  ],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		NotFoundComponent
+	],
+	imports: [
+		BrowserModule,
+		BrowserAnimationsModule,
+		FlexLayoutModule,
+		MatDialogModule, MatIconModule,
+		CoreModule,
+		ngMock,
+		AppRoutingModule
+
+	],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
